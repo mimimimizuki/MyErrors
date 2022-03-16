6bd6871637870ef70211ca9cbbee69d6100ccaa0
mimimimizuki <44560105+mimimimizuki@users.noreply.github.com>
2022-03-16 16:11:42 +0900

    todo is add testcases

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # myerrors
+
+```
+type target struct {
+    Msg string
+    ...
+    ...
+    ...
+}
+
+func (t *target) Error() string {
+    return t.Msg
+}
+
+err, ok := myerror.As[*target](err)
+
+
+```
--- myerror_test.go
+++ myerror_test.go
@@ -4,8 +4,9 @@ import (
 	// "strconv"
 	"myerror"
 	"testing"
-	"fmt"
 	"errors"
+	"strconv"
+	"github.com/stretchr/testify/require"
 )
 
 type testError struct {
@@ -26,28 +27,25 @@ func doError() *testError {
 func TestMyErrorsAs(t *testing.T) {
 	var te *testError
 
-	// _, errcase1 := strconv.Atoi("ss")
-	errcase2result := doError()
-	if ok := errors.As(errcase2result, &te); !ok {
+	_, errcase1 := strconv.Atoi("ss")
+	errcase2 := doError()
+	if ok := errors.As(errcase2, &te); !ok {
 		t.Errorf("doError()'s type is not same with testError")
 	}
 	cases := map[string] struct {
 		errorcase error
 		want bool
-		errorresult error
 	}{
-		// "strconv error": {errcase1, false, errcase1},
-		"original error": {errcase2result, true, errcase2result},
+		"strconv error": {errcase1, false},
+		"original error": {errcase2, true},
+		"": {errors.New("test"), false},
 	}
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T){
 			err, ok := myerror.As[*testError](tt.errorcase)
-			fmt.Println(err, ok)
-			if ok != tt.want {
-				t.Error(err)
-			}
-			if tt.errorresult != err {
-				t.Error(err)
+			require.Equal(t, ok, tt.want)
+			if !ok {
+				require.Nil(t, err)
 			}
 		})
 	}
